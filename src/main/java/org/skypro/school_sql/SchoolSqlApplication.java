8fe3679eca5eb3e1dd409c5d02d9a8ab3e25e446
MariaSizz <buchacik@mail.ru>
2025-07-20 19:49:07 +0300

    Initial commit.

---
+++ src/main/java/org/skypro/school_sql/SchoolSqlApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.school_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSqlApplication.class, args);
+	}
+
+}
